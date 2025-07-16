68fe85d386ceb968cf80e83096736e591d064baa
asatvik1958 <asatvik1958@gmail.com>
2025-07-16 21:54:34 +0530

    register method added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get('/register', (req, res) => {
+    res.send("Hello America")
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
