5def27de7f5831c07c782670def0dcf36c38aac5
asatvik1958 <asatvik1958@gmail.com>
2025-07-16 22:14:41 +0530

    test

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello SS!')
 })
-app.get('/register', (req, res) => {
-    res.send("Hello Changes done here")
-})
-app.get('/login', (req, res) => {
-    res.send("Hello India")
-})
 app.get('/forgotpwd', (req, res) => {
     res.send("Hellooooo")
 })
